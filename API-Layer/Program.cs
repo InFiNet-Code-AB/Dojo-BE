fce5ce33c36bf8c3c97cee1a6fccd5009bfe69c3
HubTicaBB <tijana.ilic87@protonmail.com>
2024-03-03 15:33:56 +0100

    Add format code workflow

--- API-Layer/Program.cs
+++ API-Layer/Program.cs
@@ -8,9 +8,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dojo BE", Version = "v1" });
+builder.Services.AddSwaggerGen(c =>     {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dojo BE", Version = "v1" });
 });
 
 //  Add our own services (layers) here
