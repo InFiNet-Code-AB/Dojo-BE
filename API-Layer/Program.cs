e64670cad315c75a4cc5a266c457af9f73b18ec9
HubTicaBB <tijana.ilic87@protonmail.com>
2024-03-03 15:41:56 +0100

    Unformat

--- API-Layer/Program.cs
+++ API-Layer/Program.cs
@@ -22,7 +22,11 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dojo BE API V1");
+    
+    
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", 
+        
+        "Dojo BE API V1"        );
 });
 
 app.UseHttpsRedirection();
