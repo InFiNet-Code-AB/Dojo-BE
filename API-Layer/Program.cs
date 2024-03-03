0fd109347073d4281a8a8176aa232bd93d9fbdf9
HubTicaBB <tijana.ilic87@protonmail.com>
2024-03-03 15:39:04 +0100

    Format code

--- API-Layer/Program.cs
+++ API-Layer/Program.cs
@@ -8,8 +8,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>     {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dojo BE", Version = "v1" });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dojo BE", Version = "v1" });
 });
 
 //  Add our own services (layers) here
@@ -31,7 +32,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-
