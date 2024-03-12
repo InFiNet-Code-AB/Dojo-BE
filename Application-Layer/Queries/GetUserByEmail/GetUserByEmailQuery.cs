3d99603fef3919412a387de17a4eacc61ab51d8c
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-12 23:03:39 +0100

    Added GetUserByEmailQuery and Handler

---
+++ Application-Layer/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application_Layer.Queries.GetUserByEmail
+{
+    public class GetUserByEmailQuery : IRequest<IdentityUser>
+    {
+        public string Email { get; private set; }
+        public GetUserByEmailQuery(string email)
+        {
+            Email = email;
+        }
+    }
+}
