108017c5b53309ba89d0a51deeabc0c25fcc8477
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-12 22:26:38 +0100

    added GetUserByIdQuery and Handler

---
+++ Application-Layer/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application_Layer.Queries.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<IdentityUser>
+    {
+        public string UserId { get; private set; }
+        public GetUserByIdQuery(string userId) 
+        {
+            UserId = userId;
+        }
+    }
+}
