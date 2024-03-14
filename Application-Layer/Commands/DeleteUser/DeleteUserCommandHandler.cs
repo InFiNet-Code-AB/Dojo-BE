997219bcc283ebd08cf8052707f2f20968eaa73f
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-14 11:50:24 +0100

    Created DeleteUserCommand and DeleteUserCommandHandler with RepositoryPattern. Also added a attribut in UserModel (bool IsDeleted). I had to comment out my Handler since the UserRepository hasnt been implemented yet

---
+++ Application-Layer/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain_Layer.Models.UserModel;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application_Layer.Commands.DeleteUser
+{
+    //public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+    //{
+    //    private readonly IUserRepository _userRepository;
+
+    //    public DeleteUserCommandHandler(IUserRepository userRepository)
+    //    {
+    //        _userRepository = userRepository;
+    //    }
+
+    //    public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    //    {
+    //        try
+    //        {
+    //            var user = await _userRepository.FindByIdAsync(request.UserId);
+    //            if (user == null)
+    //            {
+    //                throw new InvalidOperationException($"User with ID {request.UserId} was not found.");
+    //            }
+
+    //            var success = await _userRepository.DeleteUserAsync(user);
+    //            if (!success)
+    //            {
+    //                throw new InvalidOperationException("Failed to update the user.");
+    //            }
+
+    //            return true;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw; 
+    //        }
+    //    }
+
+    //}
+}
