bf0efdaccdf38f04ee25eecbdba33ba80b09104d
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-14 12:10:30 +0100

    Changed out manager to UserRepository. Also changed language in my error handling

--- Application-Layer/Queries/GetUserByEmail/GetUserByEmailQueryHandler.cs
+++ Application-Layer/Queries/GetUserByEmail/GetUserByEmailQueryHandler.cs
@@ -8,29 +8,36 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Application_Layer.Queries.GetUserByEmail
 {
-    public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, IdentityUser>
-    {
-        private readonly UserManager<IdentityUser> _manager;
+    //public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, IdentityUser>
+    //{
+        //private readonly IUserRepository _userRepository;
 
-        public GetUserByEmailQueryHandler(UserManager<IdentityUser> manager)
-        {
-            _manager = manager;
-        }
+        //public GetUserByEmailQueryHandler(IUserRepository userRepository)
+        //{
+        //    _userRepository = userRepository;
+        //}
 
-        public async Task<IdentityUser> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrWhiteSpace(request.Email))
-            {
-                throw new ArgumentException("E-postadressen får inte vara tom.");
-            }
+        //public async Task<IdentityUser> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        //{
+        //    if (string.IsNullOrWhiteSpace(request.Email))
+        //    {
+        //        throw new ArgumentException("Email cannot be empty!");
+        //    }
 
-            var user = await _manager.FindByEmailAsync(request.Email);
-            if (user == null)
-            {
-                throw new KeyNotFoundException($"Användaren med e-postadressen '{request.Email}' hittades inte.");
-            }
+        //    try
+        //    {
+        //        var user = await _userRepository.GetUserByEmailAsync(request.Email);
+        //        if (user == null)
+        //        {
+        //            throw new KeyNotFoundException($"User with Email '{request.Email}' cannot be found!.");
+        //        }
 
-            return user;
-        }
+        //        return user;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
     }
 }
