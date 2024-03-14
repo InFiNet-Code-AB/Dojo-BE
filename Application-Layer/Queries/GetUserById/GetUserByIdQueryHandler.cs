3c095fe46ce8c348687a894b2436b29a87136957
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-14 14:20:05 +0100

    change to repository patter in GetUserByIdQueryHandler and commented it out

--- Application-Layer/Queries/GetUserById/GetUserByIdQueryHandler.cs
+++ Application-Layer/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -3,33 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure_Layer.Repositories.User;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 
 namespace Application_Layer.Queries.GetUserById
 {
-    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, IdentityUser>
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        public GetUserByIdQueryHandler(UserManager<IdentityUser> userManager)
-        {
-            _userManager = userManager;
-        }
+    //public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, IdentityUser>
+    //{
+    //    private readonly IUserRepository _userRepository;
 
-        public async Task<IdentityUser> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrWhiteSpace(request.UserId))
-            {
-                throw new ArgumentException("UserId får inte vara tomt.");
-            }
+    //    public GetUserByIdQueryHandler(IUserRepository userRepository)
+    //    {
+    //        _userRepository = userRepository;
+    //    }
 
-            var user = await _userManager.FindByIdAsync(request.UserId);
-            if (user == null)
-            {
-                throw new KeyNotFoundException($"User with ID {request.UserId} was not found!");
-            }
+    //    public async Task<IdentityUser> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    //    {
+    //        if (string.IsNullOrWhiteSpace(request.UserId))
+    //        {
+    //            throw new ArgumentException("UserId cannot be empty.");
+    //        }
 
-            return user;
-        }
-    }
+    //        var user = await _userRepository.GetUserByIdAsync(request.UserId);
+    //        if (user == null)
+    //        {
+    //            throw new KeyNotFoundException($"User with ID {request.UserId} was not found!");
+    //        }
+
+    //        return user;
+    //    }
+    //}
 }
