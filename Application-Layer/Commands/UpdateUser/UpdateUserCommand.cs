ac8e0212d264aab3e4db36e1097d8649526f42b4
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-03-14 11:18:30 +0100

    I have create UpdatingUserDTO,UpdateUserCommand+Handler. Handler is not fuiiy implement

---
+++ Application-Layer/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application_Layer.DTO_s;
+using Domain_Layer.Models.UserModel;
+using MediatR;
+
+namespace Application_Layer.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<UserModel>
+    {
+        public UpdatingUserDTO UpdatedUser { get; }
+        public Guid Id { get; }
+        public UpdateUserCommand(UpdatingUserDTO updateUser, Guid id) 
+        {
+           UpdatedUser = updateUser;
+           Id = id;
+        }
+    }
+}
---
+++ Application-Layer/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Domain_Layer.Models.UserModel;
+using Infrastructure_Layer.Repositories.User;
+using MediatR;
+
+namespace Application_Layer.Commands.UpdateUser
+{
+    public class  UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserModel>
+    {
+        private readonly IUserRepository _userRepository;
+        public UpdateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userToUpdate = await _userRepository.GetUser();
+        }
+    }
+}
+
