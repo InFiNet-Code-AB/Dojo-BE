63b21e9348c9cac3509d0598052b429e15fc8210
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-14 12:56:35 +0100

    .

--- Application-Layer/Queries/GetAllUsers/GetAllUsersQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain_Layer.Models.UserModel;
-using MediatR;
-
-namespace Application_Layer.Queries.GetAllUsers
-{
-    public class GetAllUsersQuery : IRequest<IEnumerable<UserModel>>
-    {
-    }
-}
