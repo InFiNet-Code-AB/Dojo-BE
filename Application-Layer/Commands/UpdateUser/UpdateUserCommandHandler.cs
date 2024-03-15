14b79b60514e6e67ce4f12d249b5126dc38a3168
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-03-15 21:59:37 +0100

    UpdateUserCommandHandler is created and UpdateUserAsync is generate in IUserRepository + UserRepository

---
+++ Application-Layer/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Domain_Layer.Models.UserModel;
+using Infrastructure_Layer.Repositories.User;
+using MediatR;
+using Serilog;
+
+namespace Application_Layer.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserModel>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UpdateUserCommandHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+        public async Task<UserModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userToUpdate = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (userToUpdate == null)
+            {
+                throw new ArgumentNullException($"User with E-mail {request.Email} does not exist in the system!");
+            }
+            try
+            {
+                userToUpdate = _mapper.Map<UserModel>(request.UpdatingUserInfo);
+
+                var updatedUser = await _userRepository.UpdateUserAsync(userToUpdate);
+
+                return updatedUser;
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error("An error occurred while updating user inforamtion.", ex);
+                throw;
+            }
+        }
+    }
+}
+
+
