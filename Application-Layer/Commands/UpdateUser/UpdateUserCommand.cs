431342e6c2d920b88d3765735b388bf916a3f3fd
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-03-15 21:35:51 +0100

    UpdatingUserDTO is created

---
+++ Application-Layer/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application_Layer.DTO_s;
+using Domain_Layer.Models.UserModel;
+using MediatR;
+
+namespace Application_Layer.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<UserModel>
+    {
+        UpdatingUserDTO UpdatingUserInfo { get; }
+        string Email { get; }
+        public UpdateUserCommand(UpdatingUserDTO userToUpdate, string email) 
+        {
+            UpdatingUserInfo = userToUpdate;
+            Email = email;
+        }
+
+    }
+}
