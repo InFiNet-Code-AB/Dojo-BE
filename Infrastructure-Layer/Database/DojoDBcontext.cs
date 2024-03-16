a7450882a8b78ad2327cf6fe8a3d3fce549c249c
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-03-16 01:55:00 +0100

    I have created DbSeedHelper med some basic Data. I add DataBaseSeedHelper in DI and hade to modify DojoDbConntext in order to properly inject DatabaseSeedHelper

--- Infrastructure-Layer/Database/DojoDBcontext.cs
+++ Infrastructure-Layer/Database/DojoDBcontext.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Emit;
-using Domain_Layer.Models.UserModel;
+﻿using Domain_Layer.Models.UserModel;
 using Infrastructure_Layer.DatabaseHelper;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -8,24 +7,17 @@ namespace Infrastructure_Layer.Database
 {
     public class DojoDBContext : IdentityDbContext
     {
-        public DojoDBContext()
+        private readonly DatabaseSeedHelper _databaseSeedHelper;
+        public DojoDBContext(DbContextOptions<DojoDBContext> options, DatabaseSeedHelper databaseSeedHelper) : base(options)
         {
-
+            _databaseSeedHelper = databaseSeedHelper;
         }
-
-        public DojoDBContext(DbContextOptions<DojoDBContext> options)
-        : base(options)
-        {
-
-        }
-
         public DbSet<UserModel> User { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            DatabaseSeedHelper.SeedData(builder);
+            _databaseSeedHelper.SeedData(builder);
             base.OnModelCreating(builder);
         }
-
     }
 }
