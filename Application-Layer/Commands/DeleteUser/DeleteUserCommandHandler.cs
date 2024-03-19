526a4029532e77adb054df2699d5a8278386654a
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-19 11:59:27 +0100

    Removed outcommented code. and autogenerated related method in IUserRepository and UserRepository

--- Application-Layer/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ Application-Layer/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,46 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain_Layer.Models.UserModel;
+﻿using Infrastructure_Layer.Repositories.User;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 
 namespace Application_Layer.Commands.DeleteUser
 {
-    //public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
-    //{
-    //private readonly IUserRepository _userRepository;
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
 
-    //public DeleteUserCommandHandler(IUserRepository userRepository)
-    //{
-    //    _userRepository = userRepository;
-    //}
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
 
-    //public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
-    //{
-    //    try
-    //    {
-    //        var user = await _userRepository.GetUserByIdAsync(request.UserId);
-    //        if (user == null)
-    //        {
-    //            throw new InvalidOperationException($"User with ID {request.UserId} was not found.");
-    //        }
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserByIdAsync(request.UserId);
+                if (user == null)
+                {
+                    throw new InvalidOperationException($"User with ID {request.UserId} was not found.");
+                }
 
-    //        var success = await _userRepository.DeleteUserByIdAsync(user);
-    //        if (!success)
-    //        {
-    //            throw new InvalidOperationException("Failed to update the user.");
-    //        }
+                var success = await _userRepository.DeleteUserByIdAsync(user);
+                if (!success)
+                {
+                    throw new InvalidOperationException("Failed to delete the user.");
+                }
 
-    //        return true;
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        throw;
-    //    }
-    //}
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
 
-    //}
+    }
 }
